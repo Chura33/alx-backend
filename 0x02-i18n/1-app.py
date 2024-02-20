39a7ef4146fb0a16f0d47b5b3b988b317428e912
Chura33 <churabrady@gmail.com>
2024-02-20 11:37:12 +0000

    added 1-index.html

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -23,7 +23,8 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """returns index.html"""
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
