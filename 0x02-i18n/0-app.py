e6bdc8fcd37ce192cd507a50b276ec1cdab23c96
Chura33 <churabrady@gmail.com>
2024-02-20 10:58:02 +0000

    solved task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,13 +4,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 def index() -> str:
     """Renders the index.html template."""
     return render_template('index.html')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
